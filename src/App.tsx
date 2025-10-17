9a45845ea75610433835d787a3ab69016a651447
pkasinski <83813240+pkasinski@users.noreply.github.com>
2025-10-17 13:19:35 +0200

    Add introduction section

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Typewriter from "typewriter-effect";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [showCursor, setShowCursor] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <section className="introduction">
+        <Typewriter
+          options={{
+            cursor: "",
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Hello, my name is Pawel Kasinski.")
+              .callFunction(() => {
+                typewriter.stop();
+              })
+              .start();
+          }}
+        />
+        <p className="intro-text">
+          I am a software developer based in Gothenburg with a master's degree
+          in computer science and engineering, and work experience from Volvo
+          Group and Toyota Material Handling. My interests range from
+          algorithms, data science and backend development to UI/UX design and
+          frontend development.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+      <section className="timeline"></section>
+      <section className="projects"></section>
     </>
-  )
+  );
 }
-
-export default App
